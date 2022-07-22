39c1d3eea1d38399279bdcc7c86af02be67ed90c
Elijah Moore <65111992+elijahmoore104@users.noreply.github.com>
2022-07-22 12:12:11 +1000

    map for airports added and cleansing of locations data started

--- funcs.py
+++ funcs.py
@@ -1,6 +1,8 @@
+import string
 import requests
 import json
 import pandas as pd
+import plotly.express as px
 
 
 def jprint(obj):
@@ -18,3 +20,8 @@ def normalizeAndSaveLocal(array, file_name):
     data_pd = pd.json_normalize(array)
     data_pd.to_csv(file_name)
     return data_pd
+
+def displayCoordsOnMap(dataframe: pd.DataFrame, coords_lat: str, coords_lon: str, display_str: str, color=["blue"]):
+    fig = px.scatter_geo(dataframe, lat=coords_lat,lon=coords_lon, hover_name=display_str, color_discrete_sequence=color)
+    # fig.update_layout(title = 'Map of Airports', title_x=0.5)
+    fig.show()
--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 
-from urllib import response
 import pandas as pd
 import funcs as f 
+import plotly.express as px
 
 
 mvmts_pd = pd.read_csv("raw-data/airport-movement-data.csv")
 ports_pd = pd.read_csv("raw-data/airport-codes.csv")
-print(ports_pd)
+# print(ports_pd)
+
+# mvmts_pd = pd.DataFrame(mvmts_pd).rename(columns={"coordinates": "coords"})
+ports_pd = pd.DataFrame(ports_pd).rename(columns={"coordinates": "coords"})
+test = ports_pd.loc[
+    (ports_pd.type!="") &
+    (ports_pd.iso_country=="AU"), 
+    ["iso_country","coords", "name", "type", "municipality"]].drop_duplicates()
+
+test = test.fillna("")
+
+test["coords.lon"] = test["coords"].str.extract(r'(.*),') # left of commma
+test["coords.lat"] = test["coords"].str.extract(r',(.*)') # right of commma
+test["display_str"] = test["name"] + " | " + test["municipality"] + " | " + test["municipality"]
+
+test.to_csv('test4.csv')
+
+f.displayCoordsOnMap(
+    dataframe=test,
+    coords_lat="coords.lat", 
+    coords_lon="coords.lon", 
+    display_str="display_str")
 
