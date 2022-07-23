70abd622c8d1b9f966bad92d2b9c11a9ba219e7f
Elijah Moore <65111992+elijahmoore104@users.noreply.github.com>
2022-07-23 18:11:11 +1000

    included draft distances table

--- main.py
+++ main.py
@@ -2,7 +2,13 @@
 import pandas as pd
 import funcs as f 
 import plotly.express as px
+import geopy.distance as geo
+from itertools import permutations
 
+# coords_1 = (52.2296756, 21.0122287)
+# coords_2 = (52.406374, 16.9251681)
+
+# print (geo.geodesic(coords_1, coords_2).km)
 
 mvmts_pd = pd.read_csv("raw-data/airport-movement-data.csv")
 ports_pd = pd.read_csv("raw-data/airport-codes.csv")
@@ -10,21 +16,33 @@ ports_pd = pd.read_csv("raw-data/airport-codes.csv")
 
 # mvmts_pd = pd.DataFrame(mvmts_pd).rename(columns={"coordinates": "coords"})
 ports_pd = pd.DataFrame(ports_pd).rename(columns={"coordinates": "coords"})
-test = ports_pd.loc[
+ports_pd = ports_pd.loc[
     (ports_pd.type!="") &
     (ports_pd.iso_country=="AU"), 
     ["iso_country","coords", "name", "type", "municipality"]].drop_duplicates()
 
-test = test.fillna("")
+ports_pd = ports_pd.fillna("")
 
-test["coords.lon"] = test["coords"].str.extract(r'(.*),') # left of commma
-test["coords.lat"] = test["coords"].str.extract(r',(.*)') # right of commma
-test["display_str"] = test["name"] + " | " + test["municipality"] + " | " + test["municipality"]
+ports_pd["coords.lon"] = ports_pd["coords"].str.extract(r'(.*),') # left of commma
+ports_pd["coords.lat"] = ports_pd["coords"].str.extract(r',(.*)') # right of commma
+ports_pd["display_str"] = ports_pd["name"] + " | " + ports_pd["municipality"] + " | " + ports_pd["municipality"]
 
-test.to_csv('test4.csv')
+mvmts_pd = mvmts_pd[mvmts_pd["AIRPORT"] != "TOTAL AUSTRALIA"]
+
+mvmts_pd['Doc_In_Pct'] = mvmts_pd['Dom_Acm_In'] / mvmts_pd.groupby('Year_Ended_December')['Dom_Acm_In'].transform('sum')
+mvmts_pd['Doc_Out_Pct'] = mvmts_pd['Dom_Acm_Out'] / mvmts_pd.groupby('Year_Ended_December')['Dom_Acm_Out'].transform('sum')
+
+# Filter to 2019 to start with. A generalized solution will come later
+mvmts_pd = mvmts_pd[mvmts_pd["Year_Ended_December"] == 2019]
+
+distances_pd = pd.DataFrame( list(permutations(mvmts_pd["AIRPORT"], 2)) )
+
+distances_pd.to_csv("checks2.csv")
+mvmts_pd.to_csv('mvmts_pd.csv')
+ports_pd.to_csv('ports_pd.csv')
 
 f.displayCoordsOnMap(
-    dataframe=test,
+    dataframe=ports_pd,
     coords_lat="coords.lat", 
     coords_lon="coords.lon", 
     display_str="display_str")
