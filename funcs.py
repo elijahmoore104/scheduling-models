173bfab8d88ecb5ffada630a902fecfb8b389f23
Elijah Moore <65111992+elijahmoore104@users.noreply.github.com>
2022-07-23 18:46:38 +1000

    dropped duplicates from distances table

--- funcs.py
+++ funcs.py
@@ -1,4 +1,3 @@
-import string
 import requests
 import json
 import pandas as pd
@@ -25,3 +24,29 @@ def displayCoordsOnMap(dataframe: pd.DataFrame, coords_lat: str, coords_lon: str
     fig = px.scatter_geo(dataframe, lat=coords_lat,lon=coords_lon, hover_name=display_str, color_discrete_sequence=color)
     # fig.update_layout(title = 'Map of Airports', title_x=0.5)
     fig.show()
+
+def cleanPortsData(input_pd):
+    input_pd = pd.DataFrame(input_pd).rename(columns={"coordinates": "coords"})
+    input_pd = input_pd.loc[
+        (input_pd.type!="") &
+        (input_pd.iso_country=="AU"), 
+        ["iso_country","coords", "name", "type", "municipality"]].drop_duplicates()
+    input_pd = input_pd.fillna("")
+    input_pd["coords.lon"] = input_pd["coords"].str.extract(r'(.*),') # left of commma
+    input_pd["coords.lat"] = input_pd["coords"].str.extract(r',(.*)') # right of commma
+    input_pd["display_str"] = input_pd["name"] + " | " + input_pd["municipality"] + " | " + input_pd["municipality"]
+
+    return input_pd
+
+def cleanMovementsData(input_pd):
+        
+    input_pd = input_pd.loc[input_pd["AIRPORT"] != "TOTAL AUSTRALIA"]
+    input_pd = input_pd.copy()
+
+    input_pd['Doc_In_Pct'] = (input_pd['Dom_Acm_In'] / input_pd.groupby('Year_Ended_December')['Dom_Acm_In'].transform('sum'))
+    input_pd.loc['Doc_Out_Pct'] = input_pd['Dom_Acm_Out'] / input_pd.groupby('Year_Ended_December')['Dom_Acm_Out'].transform('sum')
+
+    # Filter to 2019 to start with. A generalized solution will come later
+    input_pd = input_pd.loc[input_pd["Year_Ended_December"] == 2019]
+
+    return input_pd
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import funcs as f 
 import plotly.express as px
@@ -7,43 +6,26 @@ from itertools import permutations
 
 # coords_1 = (52.2296756, 21.0122287)
 # coords_2 = (52.406374, 16.9251681)
-
 # print (geo.geodesic(coords_1, coords_2).km)
 
 mvmts_pd = pd.read_csv("raw-data/airport-movement-data.csv")
 ports_pd = pd.read_csv("raw-data/airport-codes.csv")
-# print(ports_pd)
 
-# mvmts_pd = pd.DataFrame(mvmts_pd).rename(columns={"coordinates": "coords"})
-ports_pd = pd.DataFrame(ports_pd).rename(columns={"coordinates": "coords"})
-ports_pd = ports_pd.loc[
-    (ports_pd.type!="") &
-    (ports_pd.iso_country=="AU"), 
-    ["iso_country","coords", "name", "type", "municipality"]].drop_duplicates()
+# storing cleaning externally to keep the file clean
+ports_pd = f.cleanPortsData(ports_pd) 
+mvmts_pd = f.cleanMovementsData(mvmts_pd)
 
-ports_pd = ports_pd.fillna("")
+port_types = ports_pd['type'].drop_duplicates()
+distances_pd = pd.DataFrame( list(permutations(mvmts_pd["AIRPORT"], 2)) ).drop_duplicates()
 
-ports_pd["coords.lon"] = ports_pd["coords"].str.extract(r'(.*),') # left of commma
-ports_pd["coords.lat"] = ports_pd["coords"].str.extract(r',(.*)') # right of commma
-ports_pd["display_str"] = ports_pd["name"] + " | " + ports_pd["municipality"] + " | " + ports_pd["municipality"]
+port_types.to_csv("clean-data/port_types.csv")
+distances_pd.to_csv("clean-data/distances_pd.csv")
+mvmts_pd.to_csv('clean-data/mvmts_pd.csv')
+ports_pd.to_csv('clean-data/ports_pd.csv')
 
-mvmts_pd = mvmts_pd[mvmts_pd["AIRPORT"] != "TOTAL AUSTRALIA"]
-
-mvmts_pd['Doc_In_Pct'] = mvmts_pd['Dom_Acm_In'] / mvmts_pd.groupby('Year_Ended_December')['Dom_Acm_In'].transform('sum')
-mvmts_pd['Doc_Out_Pct'] = mvmts_pd['Dom_Acm_Out'] / mvmts_pd.groupby('Year_Ended_December')['Dom_Acm_Out'].transform('sum')
-
-# Filter to 2019 to start with. A generalized solution will come later
-mvmts_pd = mvmts_pd[mvmts_pd["Year_Ended_December"] == 2019]
-
-distances_pd = pd.DataFrame( list(permutations(mvmts_pd["AIRPORT"], 2)) )
-
-distances_pd.to_csv("checks2.csv")
-mvmts_pd.to_csv('mvmts_pd.csv')
-ports_pd.to_csv('ports_pd.csv')
-
-f.displayCoordsOnMap(
-    dataframe=ports_pd,
-    coords_lat="coords.lat", 
-    coords_lon="coords.lon", 
-    display_str="display_str")
+# f.displayCoordsOnMap(
+#     dataframe=ports_pd,
+#     coords_lat="coords.lat", 
+#     coords_lon="coords.lon", 
+#     display_str="display_str")
 
