9fefd9a3aff9da6ee495eceee6f1bb5bc3e34b45
Elijah Moore <65111992+elijahmoore104@users.noreply.github.com>
2022-07-25 15:04:54 +1000

    add duplicate fix + scenario analysis

--- funcs.py
+++ funcs.py
@@ -1,3 +1,4 @@
+from random import sample
 import requests
 import json
 import pandas as pd
@@ -29,26 +30,27 @@ def displayCoordsOnMap(dataframe: pd.DataFrame, coords_lat: str, coords_lon: str
 def cleanPortsData(input_pd):
     input_pd = pd.DataFrame(input_pd).rename(columns={"coordinates": "coords"})
     input_pd = input_pd.loc[
-        (input_pd.type!="") &
+        (input_pd.type!="") & # currently blank but change if certain port types should be removed
         (input_pd.iso_country=="AU"), 
         ["iso_country","coords", "name", "type", "municipality"]].drop_duplicates()
     input_pd = input_pd.fillna("")
     input_pd["coords.lon"] = input_pd["coords"].str.extract(r'(.*),') # left of commma
     input_pd["coords.lat"] = input_pd["coords"].str.extract(r',(.*)') # right of commma
-    input_pd["display_str"] = input_pd["name"] + " | " + input_pd["municipality"] + " | " + input_pd["municipality"]
+    input_pd["display_str"] = input_pd["name"] + " | " + input_pd["municipality"] + " | " + input_pd["type"]
 
     return input_pd
 
 def cleanMovementsData(input_pd):
-    input_pd = input_pd.loc[input_pd["AIRPORT"] != "TOTAL AUSTRALIA"]
+    input_pd = input_pd.loc[input_pd["AIRPORT"] != "TOTAL AUSTRALIA"] #remove totals otherwise sum is doubled
     input_pd = input_pd.copy()
 
-    input_pd['Doc_In_Pct'] = (input_pd['Dom_Acm_In'] / input_pd.groupby('Year_Ended_December')['Dom_Acm_In'].transform('sum'))
-    input_pd['Doc_Out_Pct'] = (input_pd['Dom_Acm_Out'] / input_pd.groupby('Year_Ended_December')['Dom_Acm_Out'].transform('sum'))
+    input_pd['Dom_In_Pct'] = (input_pd['Dom_Acm_In'] / input_pd.groupby('Year_Ended_December')['Dom_Acm_In'].transform('sum'))
+    input_pd['Dom_Out_Pct'] = (input_pd['Dom_Acm_Out'] / input_pd.groupby('Year_Ended_December')['Dom_Acm_Out'].transform('sum'))
 
     # Filter to 2019 to start with. A generalized solution will come later
     input_pd = input_pd.loc[input_pd["Year_Ended_December"] == 2019]
     input_pd = input_pd.drop([
+        "Rank",
         "Int_Acm_In", 
         "Int_Acm_Out", 
         "Int_Acm_Total",
@@ -56,6 +58,11 @@ def cleanMovementsData(input_pd):
         "Acm_Out",
         "Acm_Total"
         ], axis=1)
+
+    input_pd['Dom_moves_per_day'] = input_pd.Dom_Acm_Total / 365
+    input_pd['Rank'] = input_pd["Dom_Acm_Total"].rank(ascending = False)
+    input_pd = input_pd.sort_values(by="Rank")
+
     return input_pd
 
 def generateRandomSet(list_of_items, distribution, attempts):
@@ -67,3 +74,27 @@ def generateRandomSet(list_of_items, distribution, attempts):
 
     # out_arr_vals = out_arr["ports"].value_counts().reset_index()
     return out_arr
+
+def randomScenarioAnalysisDuplicateCheck(mvmts_pd, yearly_volume, samples):
+    sample_list = []
+    for i in range(0,samples):
+        gen_port_calls_Out = pd.DataFrame(generateRandomSet(mvmts_pd["AIRPORT"], mvmts_pd["Doc_Out_Pct"], yearly_volume), columns=["ports"])
+        gen_port_calls_In  = pd.DataFrame(generateRandomSet(mvmts_pd["AIRPORT"], mvmts_pd["Doc_In_Pct"], yearly_volume), columns=["ports"])
+        gen_port_calls = gen_port_calls_Out.merge(gen_port_calls_In, left_index=True, right_index=True)
+        calls_amount = len(gen_port_calls)
+        duplicates_val = len(gen_port_calls.drop(gen_port_calls[gen_port_calls.ports_x != gen_port_calls.ports_y].index))
+        duplicates_pct = duplicates_val / calls_amount
+        sample_list.append([duplicates_val, duplicates_pct*100])
+        print(i, ",", round(sample_list[i][1],3), "| ", end ="")
+    print()
+    # print(sample_list)
+    
+    sample_pcts = [x[1] for x in sample_list]
+    sample_duplicates = [x[0] for x in sample_list]
+
+    print("scenarios:", samples)
+    print("average duplicates val:", np.average(sample_duplicates))
+    print("calls:", calls_amount)
+    print("average duplicates pct:", np.average(sample_pcts))
+    sample_pd = pd.DataFrame(sample_list)
+    return sample_pd
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pickletools import genops
 import pandas as pd
 import numpy as np
 import funcs as f 
@@ -19,14 +20,37 @@ mvmts_pd = f.cleanMovementsData(mvmts_pd)
 port_types = ports_pd["type"].drop_duplicates()
 distances_pd = pd.DataFrame( list(permutations(mvmts_pd["AIRPORT"], 2)) ).drop_duplicates()
 
-yearly_volume = mvmts_pd["Dom_Acm_In"].sum()
+# using realistic volumes will crash your computer! It's a lot of movments :^)
+# yearly_volume_raw = mvmts_pd["Dom_Acm_In"].sum() # 606,565 port calls in total
+yearly_volume_raw = 100000
+duplicates_moe = 0.1
+yearly_volume_inflated = int(yearly_volume_raw*(1+duplicates_moe)) #artificially inflate the yearly_volumes, since 10% are duplicates based on current match method 
 
-gen_port_calls_Out = pd.DataFrame(f.generateRandomSet(mvmts_pd["AIRPORT"], mvmts_pd["Doc_Out_Pct"], yearly_volume), columns=["ports"])
-gen_port_calls_In  = pd.DataFrame(f.generateRandomSet(mvmts_pd["AIRPORT"], mvmts_pd["Doc_In_Pct"], yearly_volume), columns=["ports"])
+# # uncomment for scenario analysis - demonstrates how many duplicates are present in the generated dataset
+# # set yearly volumes again to overide what is set by default
+# scenarios = 15
+# yearly_volume = 10000
+# duplicates_moe = 0.095
+# yearly_volume = int(yearly_volume*(1+duplicates_moe))
+# sample_list = f.randomScenarioAnalysisDuplicateCheck(mvmts_pd, yearly_volume, scenarios)
 
-gen_port_calls = gen_port_calls_Out.merge(gen_port_calls_In, left_index=True, right_index=True)
+# gen_port_calls_Out = pd.DataFrame(f.generateRandomSet(mvmts_pd["AIRPORT"], mvmts_pd["Doc_Out_Pct"], yearly_volume_inflated), columns=["ports"])
+# gen_port_calls_In  = pd.DataFrame(f.generateRandomSet(mvmts_pd["AIRPORT"], mvmts_pd["Doc_In_Pct"], yearly_volume_inflated), columns=["ports"])
+# gen_port_calls_raw = gen_port_calls_Out.merge(gen_port_calls_In, left_index=True, right_index=True)
+# duplicates_val = len(gen_port_calls_raw.drop(gen_port_calls_raw[gen_port_calls_raw.ports_x != gen_port_calls_raw.ports_y].index))
+# duplicates_pct = (duplicates_val / yearly_volume_inflated)*100
+# gen_port_calls = gen_port_calls_raw.drop(gen_port_calls_raw[gen_port_calls_raw.ports_x == gen_port_calls_raw.ports_y].index)
 
-print("port calls:", yearly_volume)
+# scenario_volume = len(gen_port_calls)
+
+# print("original port calls:", yearly_volume_raw)
+# print("inflated port calls:", yearly_volume_inflated)
+# print("inflated duplicates:", duplicates_val)
+# print("port calls after rm duplicates:", scenario_volume)
+# print("inflated duplicate pct:", round(1-scenario_volume / yearly_volume_inflated, 5)*100)
+# print("inflated accuracy:  ", round(scenario_volume / yearly_volume_inflated, 5)*100)
+# print("actual accuracy:    ", round(scenario_volume / yearly_volume_raw, 5)*100)
+
 
 # gen_port_calls_Out_vals = gen_port_calls_Out.value_counts().reset_index()
 # gen_port_calls_In_vals = gen_port_calls_In.value_counts().reset_index()
@@ -35,22 +59,20 @@ print("port calls:", yearly_volume)
 # gen_port_calls_vals = gen_port_calls_Out_vals.merge(gen_port_calls_In_vals)
 
 # gen_port_calls_values = gen_port_calls_values[gen_port_calls_values["index"] == "SYDNEY"]
-# print(gen_port_calls_values)
 
 # port_types.to_csv("clean-data/port_types.csv")
 # distances_pd.to_csv("clean-data/distances_pd.csv")
-# mvmts_pd.to_csv("clean-data/mvmts_pd.csv")
+mvmts_pd.to_csv("clean-data/mvmts_pd.csv")
 # ports_pd.to_csv("clean-data/ports_pd.csv")
 
 # gen_port_calls_Out.to_csv("clean-data/gen_port_calls_Out.csv")
 # gen_port_calls_In.to_csv("clean-data/gen_port_calls_In.csv")
 # gen_port_calls.to_csv("clean-data/gen_port_calls.csv")
-
 # gen_port_calls_Out_vals.to_csv("clean-data/gen_port_calls_Out_vals.csv")
 # gen_port_calls_In_vals.to_csv("clean-data/gen_port_calls_In_vals.csv")
 # gen_port_calls_vals.to_csv("clean-data/gen_port_calls_vals.csv")
-
 # gen_port_calls_values.to_csv("clean-data/gen_port_calls_values.csv")
+# gen_port_calls.to_csv("clean-data/gen_port_calls.csv")
 
 # f.displayCoordsOnMap(
 #     dataframe=ports_pd,
